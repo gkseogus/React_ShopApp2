0fee1d9275de185a09c5439239dd7ca85f991310
gkseogus <fbznffldj998@naver.com>
2022-05-02 11:26:05 +0900

    220502_add to input bar

--- src/components/DetailPage/index.tsx
+++ src/components/DetailPage/index.tsx
@@ -1,19 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 import shoesData from '../../data';
+
+const Inputbar = styled.input`
+    width: 200px;
+    height: 25px;
+    padding: 10px;
+`
 
 const DetailPage = () => {
 
     const [ shoesItems ] = useState(shoesData);
-    const {paramsId} = useParams();
-    
-    console.log(paramsId);
-
     const [ isAlertHide, setIsAlertHide ] = useState(false);
+    const [ inputTarget, setInputTarget ] = useState('');
+
+    const handleChange = (e: any) => {
+        const regExp = /^[0-9\b -]{0,13}$/;
+        if(regExp.test(e.target.value)){
+            setInputTarget(e.target.value);
+        };
+    };
 
     useEffect(() => {
-        setTimeout(() => { setIsAlertHide(true) }, 2000);
-    });
+        const timer = setTimeout(() => { setIsAlertHide(true) }, 2000);
+        
+        return () => {
+            clearTimeout(timer);
+        }
+    }, []);
 
     return (
         <div className="container">
@@ -25,6 +39,7 @@ const DetailPage = () => {
                     <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" alt='shoesItem'/>
                 </div>
                 <div className="col-md-6">
+                    <Inputbar placeholder="숫자만 입력하세요." type='text' value={inputTarget} onChange={handleChange}/>
                     <h4 className="pt-5">{shoesItems[0].title}</h4>
                     <p>{shoesItems[0].content}</p>
                     <p>{shoesItems[0].price}</p>
