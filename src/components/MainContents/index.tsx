f992dee492b9eb127f2e5c8cb16276df3b23f5c9
gkseogus <fbznffldj998@naver.com>
2022-05-02 11:55:35 +0900

    220502_add to more btn

--- src/components/MainContents/index.tsx
+++ src/components/MainContents/index.tsx
@@ -2,19 +2,37 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import shoesData  from '../../data.js';
+import axios from 'axios';
+import styled from 'styled-components';
+
+
+const MoreBtn = styled.button`
+    margin: 20px;
+    height: 50px;
+    width: 100px;
+`
 
 const MainContents = () => {
 
     const [ shoesItems, setShoesItems ] = useState(shoesData);
 
+    const getApi = () => {
+        axios.get('https://codingapple1.github.io/shop/data2.json')
+        .then((ApiData) => {
+            setShoesItems( [...shoesItems, ...ApiData.data]);
+        }).catch(() => {
+            alert('api 요청 실패');
+        });
+    };
+
     return (
         <div>
         {
-            shoesItems.map((items: any) => {
+            shoesItems.map((items: any, index) => {
                 return(
-                    <Container>
+                    <Container key={index}>
                         <Row>
-                            <Col xs={{ order: 12 }} >
+                            <Col xs={{ order: 12 }}>
                                 <img src={`https://codingapple1.github.io/shop/shoes${items.id}.jpg`} alt='shoesItem' width='40%'/>
                                 <h4>{items.title}</h4>
                                 <p>{items.content}</p>
@@ -24,7 +42,8 @@ const MainContents = () => {
                     </Container>
                 );
             })
-        };
+        }
+            <MoreBtn onClick={getApi}>더보기</MoreBtn>
         </div>
     );
 };
