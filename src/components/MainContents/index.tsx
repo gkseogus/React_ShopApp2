b190c749acf3c329f34b34a88155a2ac13d3a923
gkseogus <fbznffldj998@naver.com>
2022-05-02 16:42:00 +0900

    220502_main image 클릭 시 detail 페이지 이동

--- src/components/MainContents/index.tsx
+++ src/components/MainContents/index.tsx
@@ -4,6 +4,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 import shoesData  from '../../data.js';
 import axios from 'axios';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
 
 const MoreBtn = styled.button`
@@ -16,6 +17,7 @@ const MainContents = () => {
 
     const [ shoesItems, setShoesItems ] = useState(shoesData);
     const [ btnCount, setBtnCount ] = useState(0);
+    const navigate = useNavigate();
 
     const getApi = () => {
         setBtnCount(btnCount+1);
@@ -43,13 +45,15 @@ const MainContents = () => {
 
     return (
         <div>
+            <h1>사진 클릭 시 Detail 페이지로 이동</h1>
         {
             shoesItems.map((items: any, index) => {
                 return(
                     <Container key={index}>
                         <Row>
                             <Col xs={{ order: 12 }}>
-                                <img src={`https://codingapple1.github.io/shop/shoes${items.id}.jpg`} alt='shoesItem' width='40%'/>
+                                <img src={`https://codingapple1.github.io/shop/shoes${items.id}.jpg`} 
+                                alt='shoesItem' width='40%' onClick={() => { navigate('/detail') }}/>
                                 <h4>{items.title}</h4>
                                 <p>{items.content}</p>
                                 <p>{items.price}</p>
