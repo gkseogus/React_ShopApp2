9f4f1ecf376fd3d6338b02624ff02b385c477f83
gkseogus <fbznffldj998@naver.com>
2022-05-02 10:16:50 +0900

    220502_alert 창

--- src/components/DetailPage/index.tsx
+++ src/components/DetailPage/index.tsx
@@ -1,16 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import shoesData from '../../data';
 
 const DetailPage = () => {
 
-    const [ shoesItems, setShoesItems ] = useState(shoesData);
+    const [ shoesItems ] = useState(shoesData);
     const {paramsId} = useParams();
     
     console.log(paramsId);
 
+    const [ isAlertHide, setIsAlertHide ] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => { setIsAlertHide(true) }, 2000);
+    });
+
     return (
         <div className="container">
+            {
+                isAlertHide === false ?  <div className='alert alert-warning'>2초이내 구매시 할인</div> : null
+            }
             <div className="row">
                 <div className="col-md-6">
                     <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" alt='shoesItem'/>
